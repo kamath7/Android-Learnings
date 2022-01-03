bffdb4737b0a7ea2c5f2b528b58792b2ed419f77
kamath7 <adithyakamath96@gmail.com>
2022-01-04 01:13:59 +0530

    Sample app. only gives logs lol

---
+++ SampleApp/app/src/main/java/com/example/kamathinc/sampleapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.kamathinc.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void clickFunction(View view){
+        System.out.println("I am being generated! Button is being clicked successfully!");
+        Log.i("Info:","I am being generated! Button is being clicked successfully!");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
