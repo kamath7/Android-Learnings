de3276047f53dcedb057a9d9385cb047f2743b36
kamath7 <adithyakamath96@gmail.com>
2022-01-04 14:44:05 +0530

    Added simple loging form

--- SampleApp/app/src/main/java/com/example/kamathinc/sampleapp/MainActivity.java
+++ SampleApp/app/src/main/java/com/example/kamathinc/sampleapp/MainActivity.java
@@ -4,14 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public void clickFunction(View view){
-        System.out.println("I am being generated! Button is being clicked successfully!");
-        Log.i("Info:","I am being generated! Button is being clicked successfully!");
-        Toast.makeText(getApplicationContext(),"You're clicking this successfully!",Toast.LENGTH_SHORT).show();
+    public void clickFunction(View view) {
+
+        EditText usernameText = (EditText) findViewById(R.id.username);
+        EditText passwordText = (EditText) findViewById(R.id.password);
+
+        Log.i("Info:", "I am being generated! Button is being clicked successfully!");
+//        Toast.makeText(getApplicationContext(), "You're clicking this successfully!", Toast.LENGTH_SHORT).show();
+        Log.i("Values","You entered the following details - username: "+usernameText.getText().toString()+" password:"+passwordText.getText().toString());
+        Toast.makeText(getApplicationContext(), "You entered "+usernameText.getText().toString(),Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
