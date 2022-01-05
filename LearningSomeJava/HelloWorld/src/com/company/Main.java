d0f93cdb69db9a4ac8432a073b2da265af3a0c8b
kamath7 <adithyakamath96@gmail.com>
2022-01-05 16:40:29 +0530

    integers

--- LearningSomeJava/HelloWorld/src/com/company/Main.java
+++ LearningSomeJava/HelloWorld/src/com/company/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+     //Printing a simple Hello World
         System.out.println("Hello Kams!");
+
+    //variables
+        int a = 10;
+        int myFavoriteNumber = 69;
+        System.out.println("My fav number "+myFavoriteNumber);
+
     }
 }
