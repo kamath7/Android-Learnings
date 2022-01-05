bd738a166bc412351a4b476bc378dc4b7f2eb63d
kamath7 <adithyakamath96@gmail.com>
2022-01-05 21:36:03 +0530

    If and else added

---
+++ LearningSomeJava/Conditionals/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a =19;
+
+
+        if ( 69 > 1){
+            System.out.println("Elite number that");
+        }else{
+            System.out.println("GTFO");
+        }
+
+        if ( a > 18){
+            System.out.println("You are an adult!");
+        }else{
+            System.out.println("Head back and drink bournvita");
+        }
+    }
+}
