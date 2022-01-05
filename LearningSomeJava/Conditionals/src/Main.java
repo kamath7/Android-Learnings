c4e63b87634555c2f6d97c2beef0bdfdb59cd593
kamath7 <adithyakamath96@gmail.com>
2022-01-05 21:41:16 +0530

    added more conditionals

--- LearningSomeJava/Conditionals/src/Main.java
+++ LearningSomeJava/Conditionals/src/Main.java
@@ -16,5 +16,21 @@ public class Main {
         }else{
             System.out.println("Head back and drink bournvita");
         }
+
+        int sal = 20000;
+
+        if (sal > 50000){
+            System.out.println("You are rich");
+        }else if (sal < 50000 && sal > 15000){
+            System.out.println("Earning enough");
+        }
+
+        int[] someArr = {69,71};
+        if (someArr[1] > someArr[0]){
+            System.out.println("Second element greater than the 1st");
+        }else{
+            System.out.println("First element elite");
+        }
+
     }
 }
