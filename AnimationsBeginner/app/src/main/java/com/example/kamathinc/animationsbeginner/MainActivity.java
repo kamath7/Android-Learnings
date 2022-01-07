deca8b24078c3d7d8225a9f0b84de7698dc88755
kamath7 <adithyakamath96@gmail.com>
2022-01-07 16:38:03 +0530

    app complete

--- AnimationsBeginner/app/src/main/java/com/example/kamathinc/animationsbeginner/MainActivity.java
+++ AnimationsBeginner/app/src/main/java/com/example/kamathinc/animationsbeginner/MainActivity.java
@@ -7,11 +7,29 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    public void doAFade(View view){
-        Log.i("INFO","Image view was tapped!");
+    boolean isPeterAlive = false;
+
+    public void doAFade(View view) {
+        Log.i("INFO", "Image view was tapped!");
+
         ImageView imageView = findViewById(R.id.imageView);
-//        imageView.animate().alpha(0).setDuration(2000); //Fade out effect
+        ImageView imageView1 = findViewById(R.id.imageView1);
+
+
+        if (isPeterAlive == false){
+
+            imageView.animate().alpha(0).setDuration(2000);
+            imageView1.animate().alpha(1).setDuration(2000);
+            isPeterAlive = true;
+        }else{
+            //Fade out effect
+            imageView1.animate().alpha(0).setDuration(2000);
+            imageView.animate().alpha(1).setDuration(2000);
+            isPeterAlive = false;
+
+        }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
