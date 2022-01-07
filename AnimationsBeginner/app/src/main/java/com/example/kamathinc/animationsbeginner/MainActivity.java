6d5adf32db2c978d93668bf68d2f7ceafcb3b95d
kamath7 <adithyakamath96@gmail.com>
2022-01-07 16:30:34 +0530

    some animations

--- AnimationsBeginner/app/src/main/java/com/example/kamathinc/animationsbeginner/MainActivity.java
+++ AnimationsBeginner/app/src/main/java/com/example/kamathinc/animationsbeginner/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public void doAFade(View view){
         Log.i("INFO","Image view was tapped!");
+        ImageView imageView = findViewById(R.id.imageView);
+//        imageView.animate().alpha(0).setDuration(2000); //Fade out effect
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
