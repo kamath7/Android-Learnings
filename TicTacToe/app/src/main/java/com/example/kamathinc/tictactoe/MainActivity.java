0fbe8cc85eb0b36de599f8b4c24368e27baf059b
kamath7 <adithyakamath96@gmail.com>
2022-01-10 22:45:11 +0530

    some initial backend

--- TicTacToe/app/src/main/java/com/example/kamathinc/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/kamathinc/tictactoe/MainActivity.java
@@ -6,11 +6,21 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    //to keep track of players. 0 - yellow 1  - red
+    int activePlayer = 0 ;
 
     public void dropIn (View view){
         ImageView counter = (ImageView) view;
         counter.setTranslationY(-1500);
+
+    if(activePlayer == 0 ){
         counter.setImageResource(R.drawable.yellow);
+        activePlayer = 1;
+
+    }else{
+        counter.setImageResource(R.drawable.red);
+        activePlayer = 0;
+    }
         counter.animate().translationYBy(1500).rotation(2600).setDuration(300);
     }
     @Override
