07e3049a90ae952c1b10ce836f03367231f83875
kamath7 <adithyakamath96@gmail.com>
2022-01-10 23:02:32 +0530

    winner can now be shown

--- TicTacToe/app/src/main/java/com/example/kamathinc/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/kamathinc/tictactoe/MainActivity.java
@@ -5,14 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //to keep track of players. 0 - yellow 1  - red
-    int activePlayer = 0 ;
-    int[]gameState = {2,2,2,2,2,2,2,2,2}; //why 2? - means the square is untapped yet
-    int[][]winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2}; //why 2? - means the square is untapped yet
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}}; //winning positions possible
 
-    public void dropIn (View view){
+    public void dropIn(View view) {
 
         ImageView counter = (ImageView) view;
 //        Log.i("Info","Counter "+counter.getTag().toString());
@@ -21,16 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
         counter.setTranslationY(-1500);
 
-    if(activePlayer == 0 ){
-        counter.setImageResource(R.drawable.yellow);
-        activePlayer = 1;
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
 
-    }else{
-        counter.setImageResource(R.drawable.red);
-        activePlayer = 0;
-    }
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
         counter.animate().translationYBy(1500).rotation(2600).setDuration(300);
+        for (int[] winningPosition : winningPositions) {
+            if ((gameState[winningPosition[0]] == gameState[winningPosition[1]]) && (gameState[winningPosition[1]] == gameState[winningPosition[2]]) && (gameState[winningPosition[0]] != 2)) {
+
+                String winner = " ";
+                if (activePlayer == 1){
+                    winner = "Yellow!";
+                }else{
+                 winner = "Red";
+                }
+                Toast.makeText(this, "Winner is "+winner, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
