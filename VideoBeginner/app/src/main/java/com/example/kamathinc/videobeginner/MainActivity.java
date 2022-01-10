d52aec69c7ffce9ad7de31215b0e3037d3f912ee
kamath7 <adithyakamath96@gmail.com>
2022-01-11 00:10:55 +0530

    Video complete. starting with audio

--- VideoBeginner/app/src/main/java/com/example/kamathinc/videobeginner/MainActivity.java
+++ VideoBeginner/app/src/main/java/com/example/kamathinc/videobeginner/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.kamathinc.videobeginner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://"+ getPackageName()+"/"+R.raw.myvideo);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
