3d427824427da4c07719112cbd743acbc644faa8
kamath7 <adithyakamath96@gmail.com>
2022-01-10 22:51:43 +0530

    setting up logic for winning positions

--- TicTacToe/app/src/main/java/com/example/kamathinc/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/kamathinc/tictactoe/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.kamathinc.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     //to keep track of players. 0 - yellow 1  - red
     int activePlayer = 0 ;
+    int[]gameState = {2,2,2,2,2,2,2,2,2}; //why 2? - means the square is untapped yet
+    int[][]winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     public void dropIn (View view){
+
         ImageView counter = (ImageView) view;
+//        Log.i("Info","Counter "+counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedCounter] = activePlayer;
+
         counter.setTranslationY(-1500);
 
     if(activePlayer == 0 ){
