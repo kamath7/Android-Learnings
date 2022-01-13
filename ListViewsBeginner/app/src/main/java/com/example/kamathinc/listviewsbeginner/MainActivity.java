8cb4e45aae45867d42c695f9ce9654abeb3f5604
kamath7 <adithyakamath96@gmail.com>
2022-01-13 14:55:17 +0530

    initial playing around

--- ListViewsBeginner/app/src/main/java/com/example/kamathinc/listviewsbeginner/MainActivity.java
+++ ListViewsBeginner/app/src/main/java/com/example/kamathinc/listviewsbeginner/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kamathinc.listviewsbeginner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
         myFamilyMembers.add("Rooney");
         myFamilyMembers.add("Roy Keane");
         myFamilyMembers.add("Nemanja Vidic");
-    
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamilyMembers); //simple_list_item_1 is a type of view for the list
+        listView.setAdapter(arrayAdapter);
 
     }
 }
