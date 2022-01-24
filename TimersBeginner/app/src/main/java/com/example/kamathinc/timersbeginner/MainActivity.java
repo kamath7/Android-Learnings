e9248c69e3807a81e9854a56c60892cb28a76f26
kamath7 <adithyakamath96@gmail.com>
2022-01-25 01:05:57 +0530

    Timer done

--- TimersBeginner/app/src/main/java/com/example/kamathinc/timersbeginner/MainActivity.java
+++ TimersBeginner/app/src/main/java/com/example/kamathinc/timersbeginner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kamathinc.timersbeginner;
 
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,14 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Handler handler = new Handler();
-        Runnable run = new Runnable() {
-            @Override
-            public void run() {
-                Log.i("Something ran","Second passed");
-                handler.postDelayed(this, 1000); //will be run after every second 
-            }
-        };
+        new CountDownTimer(10000, 1000){
+            //duration of 10 secs with runs every second
+          public void onTick(long timeTillDone){
+            Log.i("Seconds left", String.valueOf(timeTillDone/1000));
+          }
+          public void onFinish(){
+              Log.i("Done!", "Countdown completed");
+          }
+        }.start();
+
+//        final Handler handler = new Handler();
+//        Runnable run = new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.i("Something ran","Second passed");
+//                handler.postDelayed(this, 1000); //will be run after every second
+//            }
+//        };
+//        handler.post(run);
 
     }
 }
