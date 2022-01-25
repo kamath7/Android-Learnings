ed27f93794fd60ff80e0698befd4b3afb0ad083c
kamath7 <adithyakamath96@gmail.com>
2022-01-25 20:46:19 +0530

    initial setup

--- NumberTrainer/app/src/main/java/com/example/kamathinc/numbertrainer/MainActivity.java
+++ NumberTrainer/app/src/main/java/com/example/kamathinc/numbertrainer/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.kamathinc.numbertrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    public void gameStart(View view){
+        startButton.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.goButton);
     }
 }
