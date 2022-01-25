7a4b12d783c491a8ff9187a85d36e649945a6a36
kamath7 <adithyakamath96@gmail.com>
2022-01-25 14:00:07 +0530

    adding noise on finish

--- AVeryGoodTimer/app/src/main/java/com/example/kamathinc/averygoodtimer/MainActivity.java
+++ AVeryGoodTimer/app/src/main/java/com/example/kamathinc/averygoodtimer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kamathinc.averygoodtimer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,37 +12,40 @@ public class MainActivity extends AppCompatActivity {
 
     TextView timerTextView;
     SeekBar timerSeekBar;
-    public void buttonClicked(View view){
+
+    public void buttonClicked(View view) {
         //max timer is user put time *  1000 .meaning 10 mins and interval of 1 second
-        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000, 1000)  {
+        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
             @Override
             public void onTick(long l) {
-                updateTimer((int)l/1000);
+                updateTimer((int) l / 1000);
             }
 
             @Override
             public void onFinish() {
-
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                mediaPlayer.start();
             }
         }.start();
     }
 
-    public void updateTimer(int secondsLeft){
-        int minutes = secondsLeft / 60 ;
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
         int seconds = secondsLeft - (minutes * 60);
         String secondsStr = Integer.toString(seconds);
 
-        if (seconds <=9  ){
-            secondsStr = "0"+secondsStr;
+        if (seconds <= 9) {
+            secondsStr = "0" + secondsStr;
         }
-        timerTextView.setText(Integer.toString(minutes)+ ":"+ secondsStr);
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondsStr);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerSeekBar = findViewById(R.id.timerSeekBar);
         timerTextView = findViewById(R.id.timerTextView);
 
         int max = 600; //max 10 mins. 10 * 60
