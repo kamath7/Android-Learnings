fd9d47996f01a2a624e918539759c06a14073803
kamath7 <adithyakamath96@gmail.com>
2022-01-25 21:21:29 +0530

    initial setup

--- NumberTrainer/app/src/main/java/com/example/kamathinc/numbertrainer/MainActivity.java
+++ NumberTrainer/app/src/main/java/com/example/kamathinc/numbertrainer/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    TextView problemText;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
 
     public void gameStart(View view){
         startButton.setVisibility(View.INVISIBLE);
@@ -17,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         startButton = findViewById(R.id.goButton);
+        problemText = findViewById(R.id.problemText);
+
+        Random random = new Random();
+
+        int a = random.nextInt(26);
+        int b = random.nextInt(26);
+
+        problemText.setText(Integer.toString(a) + "+" + Integer.toString(b));
     }
 }
