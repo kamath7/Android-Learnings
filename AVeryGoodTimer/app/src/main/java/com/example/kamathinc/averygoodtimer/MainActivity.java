d47ebb05b1c024b852a4ad4c2ae5531798b34592
kamath7 <adithyakamath96@gmail.com>
2022-01-25 14:11:59 +0530

    timer complete

--- AVeryGoodTimer/app/src/main/java/com/example/kamathinc/averygoodtimer/MainActivity.java
+++ AVeryGoodTimer/app/src/main/java/com/example/kamathinc/averygoodtimer/MainActivity.java
@@ -15,25 +15,44 @@ public class MainActivity extends AppCompatActivity {
     SeekBar timerSeekBar;
     Boolean activeTimer = false;
     Button timerButton;
+    CountDownTimer countDownTimer;
+
+
+    public void resetTimer() {
+        timerTextView.setText("0:39");
+        timerSeekBar.setProgress(39);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        timerButton.setText("GO");
+        activeTimer = false;
+    }
 
     public void buttonClicked(View view) {
-        //max timer is user put time *  1000 .meaning 10 mins and interval of 1 second
-        activeTimer = true;
-        timerSeekBar.setEnabled(false);
-        timerButton.setText("STOP");
-        
-        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
-            @Override
-            public void onTick(long l) {
-                updateTimer((int) l / 1000);
-            }
 
-            @Override
-            public void onFinish() {
-                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
-                mediaPlayer.start();
-            }
-        }.start();
+        if (activeTimer) {
+            resetTimer();
+        } else {
+            activeTimer = true;
+            timerSeekBar.setEnabled(false);
+            timerButton.setText("STOP");
+            //max timer is user put time *  1000 .meaning 10 mins and interval of 1 second
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long l) {
+                    updateTimer((int) l / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mediaPlayer.start();
+                    resetTimer();
+
+                }
+            }.start();
+        }
+
     }
 
     public void updateTimer(int secondsLeft) {
