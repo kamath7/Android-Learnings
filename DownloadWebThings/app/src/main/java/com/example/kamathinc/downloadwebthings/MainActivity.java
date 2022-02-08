d40e93d487966fb7c3d996449c56628876288a82
kamath7 <adithyakamath96@gmail.com>
2022-02-08 23:52:52 +0530

    url working as usual

--- DownloadWebThings/app/src/main/java/com/example/kamathinc/downloadwebthings/MainActivity.java
+++ DownloadWebThings/app/src/main/java/com/example/kamathinc/downloadwebthings/MainActivity.java
@@ -5,14 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     public class Downloader extends AsyncTask<String,Void, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            Log.i("URL : ",strings[0]);
-            return "Done!";
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStreamReader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                return result;
+
+            }catch(Exception e){
+                e.getStackTrace().toString();
+                return "Something went wrong";
+            }
+
         }
     }
 
@@ -28,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
         catch(Exception e){
-            Log.i("Error", "Error "+e.getStackTrace() );
+            Log.i("Error", "Error "+e.getStackTrace().toString() );
         }
 
         Log.i("Result - ", result);
