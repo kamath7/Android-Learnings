f1330ca41ca7bb0ec9a588f7652dbb4e6132b785
kamath7 <adithyakamath96@gmail.com>
2022-02-09 00:54:42 +0530

    substring added

--- LearningSomeJava/StringManipulation/src/Main.java
+++ LearningSomeJava/StringManipulation/src/Main.java
@@ -6,5 +6,12 @@ public class Main {
        String myBigString = "the quick brown fox jumped over the lazy dog";
         String [] splitString = myBigString.split(" ");
         System.out.println(Arrays.toString(splitString));
+
+        String myBigString2 = "Mississippi";
+        System.out.println(Arrays.toString(myBigString2.split("s")));
+
+        String myBigString3 = "Palak Paneer";
+        System.out.println(myBigString3.substring(6));
+        System.out.println(myBigString2.substring(1,5));
     }
 }
