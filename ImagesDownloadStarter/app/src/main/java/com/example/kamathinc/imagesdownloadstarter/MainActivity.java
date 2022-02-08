06187c2e86af3a5cf93df8aa24ea9858e114ac02
kamath7 <adithyakamath96@gmail.com>
2022-02-09 00:36:30 +0530

    app complete

--- ImagesDownloadStarter/app/src/main/java/com/example/kamathinc/imagesdownloadstarter/MainActivity.java
+++ ImagesDownloadStarter/app/src/main/java/com/example/kamathinc/imagesdownloadstarter/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.kamathinc.imagesdownloadstarter;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,7 +18,16 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
 
     public void loadImage(View view){
+        ImageDownloader imageDownloader = new ImageDownloader();
+        Bitmap myImage;
+        try{
 
+            myImage = imageDownloader.execute("https://upload.wikimedia.org/wikipedia/en/thumb/7/77/EricCartman.png/250px-EricCartman.png").get();
+            imageView.setImageBitmap(myImage);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -31,8 +44,17 @@ public class MainActivity extends AppCompatActivity {
         protected Bitmap doInBackground(String... urls) {
             try {
                 URL url  = new URL(urls[0]);
-                
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                return bitmap;
             } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
