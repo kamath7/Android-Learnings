2a7485bf670f4092f67b363dc77bb4f01ef441d2
kamath7 <adithyakamath96@gmail.com>
2022-02-09 20:45:29 +0530

    initial setup done

---
+++ CelebGuesser/app/src/main/java/com/example/kamathinc/celebguesser/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kamathinc.celebguesser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// Use https://www.imdb.com/list/ls052283250/
