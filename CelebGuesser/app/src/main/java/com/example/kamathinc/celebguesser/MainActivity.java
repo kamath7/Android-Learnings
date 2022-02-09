206ecdeb79e1730c328a318e6766600a57f99ccd
kamath7 <adithyakamath96@gmail.com>
2022-02-10 01:36:34 +0530

    getting the set of urls and names

--- CelebGuesser/app/src/main/java/com/example/kamathinc/celebguesser/MainActivity.java
+++ CelebGuesser/app/src/main/java/com/example/kamathinc/celebguesser/MainActivity.java
@@ -4,22 +4,32 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+
+    
 
     public class Downloader extends AsyncTask<String, Void, String>{
 
         @Override
         protected String doInBackground(String... urls) {
 
-            String result = "";
+            StringBuilder stringBuilder = new StringBuilder();
             URL url;
-            HttpURLConnection urlConnection = null;
+            HttpURLConnection  urlConnection = null;
 
             try{
                 url = new URL(urls[0]);
@@ -30,10 +40,10 @@ public class MainActivity extends AppCompatActivity {
                 int data = inputStreamReader.read();
                 while(data != -1){
                     char curr = (char)data;
-                    result += curr;
+                    stringBuilder.append(curr);
                     data = inputStreamReader.read();
                 }
-            return result;
+            return stringBuilder.toString();
 
             }catch(Exception e){
 
@@ -54,8 +64,21 @@ public class MainActivity extends AppCompatActivity {
         String result = null;
         try{
 
-            result = downloader.execute("https://www.imdb.com/list/ls052283250/").get();
+            result = downloader.execute("https://www.imdb.com/list/ls052283250").get();
 
+            String[] spltResult = result.split("<div class=\"article listo\">");
+
+            Pattern p = Pattern.compile("src=\"(.*?).jpg\"");
+            Matcher m = p.matcher(result);
+            while(m.find()){
+                celebURLs.add(m.group(1)+".jpg");
+            }
+            Log.i("Some URL", "Message - "+celebURLs);
+            p=Pattern.compile("<img alt=\"(.*?)\"");
+            m=p.matcher(result);
+            while(m.find()){
+                celebNames.add(m.group(1));
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
