ce807a65ecce6cd54bdc5bebe9a013979330871d
kamath7 <adithyakamath96@gmail.com>
2022-02-10 14:08:47 +0530

    initial setup

---
+++ JSONExperiment/app/src/main/java/com/example/kamathinc/jsonexperiment/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.kamathinc.jsonexperiment;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    public class Downloader extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection connection = null;
+            try{
+
+                url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                InputStream in = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data !=-1){
+                    char current = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+//            Log.i("JSON OP", s);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Downloader downloader = new Downloader();
+        try{
+            downloader.execute("https://catfact.ninja/fact").get();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
