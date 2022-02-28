03827f26d7765607f0e0966eba25cf48ec9094ef
kamath7 <adithyakamath96@gmail.com>
2022-03-01 00:41:01 +0530

    bouncing between screens

--- MultipleScreensLearning/app/src/main/java/com/example/kamathinc/multiplescreenslearning/SecondActivity.java
+++ MultipleScreensLearning/app/src/main/java/com/example/kamathinc/multiplescreenslearning/SecondActivity.java
@@ -1,9 +1,16 @@
 package com.example.kamathinc.multiplescreenslearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public void goBack(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
