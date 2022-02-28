939c6c5d4d8bb6935bffeafb6fa9a7dfe93c578b
kamath7 <adithyakamath96@gmail.com>
2022-03-01 00:58:42 +0530

    setting up listView

--- MultipleScreensLearning/app/src/main/java/com/example/kamathinc/multiplescreenslearning/MainActivity.java
+++ MultipleScreensLearning/app/src/main/java/com/example/kamathinc/multiplescreenslearning/MainActivity.java
@@ -4,18 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+//
+//    public void goScreen(View view){
+//        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+//        intent.putExtra("age", 25); //to share infor
+//        startActivity(intent);
+//    }
 
-    public void goScreen(View view){
-        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        intent.putExtra("age", 25); //to share infor
-        startActivity(intent);
-    }
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        ArrayList<String> dishes = new ArrayList<String>();
+
+        dishes.add("Palak Paneer");
+        dishes.add("Pulav");
+        dishes.add("Gobi Manchurian");
+        dishes.add("Rasmalai");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1 , dishes);
+        listView.setAdapter(arrayAdapter);
+
+        
+
     }
 }
