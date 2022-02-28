c884e08dd05754caff8f9774d9c6b01050fd92ae
kamath7 <adithyakamath96@gmail.com>
2022-03-01 01:01:29 +0530

    setting up intent with extra

--- MultipleScreensLearning/app/src/main/java/com/example/kamathinc/multiplescreenslearning/MainActivity.java
+++ MultipleScreensLearning/app/src/main/java/com/example/kamathinc/multiplescreenslearning/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,23 +19,32 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
     ListView listView;
+    ArrayList<String> dishes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
-        ArrayList<String> dishes = new ArrayList<String>();
+        dishes = new ArrayList<String>();
 
         dishes.add("Palak Paneer");
         dishes.add("Pulav");
         dishes.add("Gobi Manchurian");
         dishes.add("Rasmalai");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1 , dishes);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, dishes);
         listView.setAdapter(arrayAdapter);
 
-        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("Dish Name", dishes.get(position));
+                startActivity(intent);
+            }
+        });
 
     }
 }
