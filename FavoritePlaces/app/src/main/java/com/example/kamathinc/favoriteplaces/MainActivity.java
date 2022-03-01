f6b9a69b1e9360597ad665732ecc8edd8077ddd9
kamath7 <adithyakamath96@gmail.com>
2022-03-01 13:18:44 +0530

    onclick listener added

--- FavoritePlaces/app/src/main/java/com/example/kamathinc/favoriteplaces/MainActivity.java
+++ FavoritePlaces/app/src/main/java/com/example/kamathinc/favoriteplaces/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.kamathinc.favoriteplaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView llstView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        llstView = findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
 
         ArrayList<String> favPlaces = new ArrayList<String>();
-        
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, favPlaces);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, Integer.toString(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
