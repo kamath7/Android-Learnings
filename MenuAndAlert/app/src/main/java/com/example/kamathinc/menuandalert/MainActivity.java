b46e5414e26b05d00b8dbab8cf8196b2822f75f3
kamath7 <adithyakamath96@gmail.com>
2022-03-04 02:24:15 +0530

    alert dialog done

--- MenuAndAlert/app/src/main/java/com/example/kamathinc/menuandalert/MainActivity.java
+++ MenuAndAlert/app/src/main/java/com/example/kamathinc/menuandalert/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    SharedPreferences sharedPreferences
+    SharedPreferences sharedPreferences;
     public  void setLanguage(String language){
         sharedPreferences.edit().putString("language",language).apply();
         textView.setText("Language set to "+language);
@@ -21,27 +21,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sharedPreferences = this.getSharedPreferences("com.example.kamathinc.menuandaler", Context.MODE_PRIVATE);
 
+        sharedPreferences = this.getSharedPreferences("com.example.kamathinc.menuandaler", Context.MODE_PRIVATE);
         textView = findViewById(R.id.textView);
 
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle("Preferred Language")
-                .setMessage("Which Language?")
-                .setPositiveButton("English", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        setLanguage("English");
-                        Toast.makeText(MainActivity.this, "You are!", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("Hindi", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        setLanguage("Hindi");
-                    }
-                })
-                .show();
+        String language = sharedPreferences.getString("language","error!");
+
+        if (language.equals("error!")){
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Preferred Language")
+                    .setMessage("Which Language?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("English");
+                            Toast.makeText(MainActivity.this, "You are!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("Hindi", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("Hindi");
+                        }
+                    })
+                    .show();
+        }else{
+            textView.setText(language);
+        }
+
+
     }
 }
