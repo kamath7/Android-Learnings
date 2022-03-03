52b2c07e081ad20f6f4cc230810d5be2122d4586
kamath7 <adithyakamath96@gmail.com>
2022-03-04 00:16:34 +0530

    using object serialiser

--- LocalStorageLearning/app/src/main/java/com/example/kamathinc/localstoragelearning/MainActivity.java
+++ LocalStorageLearning/app/src/main/java/com/example/kamathinc/localstoragelearning/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,8 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.kamathinc.localstoragelearning", Context.MODE_PRIVATE);
-        sharedPreferences.edit().putString("userName", "kamath7").apply(); //ading a new element
-        String retrievedUsername = sharedPreferences.getString("userName","undefined");
-        Log.i("Log Info",retrievedUsername);
+//        sharedPreferences.edit().putString("userName", "kamath7").apply(); //ading a new element
+//        String retrievedUsername = sharedPreferences.getString("userName","undefined");
+//        Log.i("Log Info",retrievedUsername);
+
+        ArrayList<String> myFavFoods = new ArrayList<String>();
+        myFavFoods.add("Palak Paneer");
+        myFavFoods.add("Pulav");
+        myFavFoods.add("Aloo Pakoda");
+        myFavFoods.add("Rasmalai");
+
+        sharedPreferences.
+
     }
 }
