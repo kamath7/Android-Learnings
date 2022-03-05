56e95dfcb5a2760050a8cd76a4a9c1bba8e6aa7a
kamath7 <adithyakamath96@gmail.com>
2022-03-05 23:20:22 +0530

    fetching data from the db

--- SQLLearning/app/src/main/java/com/example/kamathinc/sqllearning/MainActivity.java
+++ SQLLearning/app/src/main/java/com/example/kamathinc/sqllearning/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.kamathinc.sqllearning;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         myDb.execSQL("INSERT INTO books (name,author) VALUES ('And Then There Were None', 'Agatha Christie')");
         myDb.execSQL("INSERT INTO books (name,author) VALUES ('Master of the game', 'Sidney Sheldon')");
 
+        Cursor cursor = myDb.rawQuery("SELECT * FROM books",null);
+
+        int nameIndex = cursor.getColumnIndex("name");
+        int authorIndex = cursor.getColumnIndex("author");
+
+        cursor.moveToFirst();
+
+        while (cursor !=null){
+
+            Log.i("SQL Query-Name", cursor.getString(nameIndex));
+            Log.i("SQL Query-Author", cursor.getString(authorIndex));
+
+            cursor.moveToNext();
+        }
     }
 }
