24aeae17f23e5423dbe9668693a6e1c7b4bd121f
kamath7 <adithyakamath96@gmail.com>
2022-03-06 01:19:57 +0530

    getting article info

--- MyNewsReader/app/src/main/java/com/example/kamathinc/mynewsreader/MainActivity.java
+++ MyNewsReader/app/src/main/java/com/example/kamathinc/mynewsreader/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -62,7 +64,32 @@ public class MainActivity extends AppCompatActivity {
                     data = inputStreamReader.read();
                 }
 
-                Log.i("URL Content", result );
+                JSONArray jsonArray = new JSONArray(result);
+                int articleLimit = 17;
+
+                if (jsonArray.length() < 17 ){
+                    articleLimit = jsonArray.length();
+                }
+
+                for (int  i =0 ; i < articleLimit;  i++){
+                    String articleId = jsonArray.getString(i);
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/"+articleId+".json?print=pretty");
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    inputStream = httpURLConnection.getInputStream();
+                    inputStreamReader = new InputStreamReader(inputStream);
+
+                    data = inputStreamReader.read();
+                    String articleInfo = "";
+                    while(data != -1){
+                        char current = (char)data;
+                        articleInfo += current;
+                        data = inputStreamReader.read();
+                    }
+
+                    Log.i("Article Info", articleInfo);
+                }
+
+//                Log.i("URL Content", result );
                 return result;
             }catch(Exception e){
                 e.printStackTrace();
