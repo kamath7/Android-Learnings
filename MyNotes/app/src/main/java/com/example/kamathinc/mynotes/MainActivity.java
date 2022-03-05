e63d4838f85786c60e97d66637db8eac619ed5db
kamath7 <adithyakamath96@gmail.com>
2022-03-05 18:47:10 +0530

    edit note activity

--- MyNotes/app/src/main/java/com/example/kamathinc/mynotes/MainActivity.java
+++ MyNotes/app/src/main/java/com/example/kamathinc/mynotes/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kamathinc.mynotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), EditNoteActivity.class);
+                intent.putExtra("noteId",position);
+                startActivity(intent);
+            }
+        });
 
     }
 }
