da1619f744027fede599a4a6d9c000013e1698d8
kamath7 <adithyakamath96@gmail.com>
2022-03-05 20:41:27 +0530

    delete on long click

--- MyNotes/app/src/main/java/com/example/kamathinc/mynotes/MainActivity.java
+++ MyNotes/app/src/main/java/com/example/kamathinc/mynotes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kamathinc.mynotes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -36,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), EditNoteActivity.class);
                 intent.putExtra("noteId",position);
                 startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int itemToDeleteIndex = position;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Delete Note")
+                        .setMessage("Are you sure you want to delete?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(itemToDeleteIndex);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null).show();
+
+                return true;
             }
         });
     }
