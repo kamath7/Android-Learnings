5a2b893316bb24b2a0c57193728a10ed18aa7f50
kamath7 <adithyakamath96@gmail.com>
2022-03-06 01:00:43 +0530

    url object created

--- MyNewsReader/app/src/main/java/com/example/kamathinc/mynewsreader/MainActivity.java
+++ MyNewsReader/app/src/main/java/com/example/kamathinc/mynewsreader/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.kamathinc.mynewsreader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -22,5 +29,35 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setAdapter(arrayAdapter);
 
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+
+            try{
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while(data != -1){
+                    char current = (char)data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
