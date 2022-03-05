6371fd167222f8777bf41cd70173dd12ee0827d4
kamath7 <adithyakamath96@gmail.com>
2022-03-06 00:10:12 +0530

    loading custom html in the webview

--- WebviewDemo/app/src/main/java/com/example/kamathinc/webviewdemo/MainActivity.java
+++ WebviewDemo/app/src/main/java/com/example/kamathinc/webviewdemo/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com");
-
+        //webView.loadUrl("https://www.google.com");
+        //loading raw html
+        webView.loadData("<html> <body><h1> This is a great website </h1 <p> Check more of me </p>> </body></html>", "text/html","UTF-8");
     }
 }
