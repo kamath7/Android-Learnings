09c319cd9bf9cce26ea221f5d5e41cb9604d08ec
kamath7 <adithyakamath96@gmail.com>
2022-03-14 20:22:09 +0530

    variables

---
+++ Kotlin/Variables/src/Variables.kt
@@ -0,0 +1,14 @@
+class Variables {
+}
+
+fun main() {
+    var age = 25; //mutable
+
+    val name = "Adithya" //immutable
+
+    var amount: Int = 30000;
+
+    var favDish: String = "Palak Paneer"
+
+    println("I am $name. My age is $age. I spent $amount on Video Games. My favorite dish is $favDish")
+}
