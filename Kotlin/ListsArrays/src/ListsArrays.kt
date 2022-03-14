5bd316acc576bec88c6c967591298cd669ab16d7
kamath7 <adithyakamath96@gmail.com>
2022-03-14 20:40:42 +0530

    lists and arrays

---
+++ Kotlin/ListsArrays/src/ListsArrays.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var someNums = arrayOf<Int>()
+
+    var someNums2 = mutableListOf(1,2,3)
+
+    println(someNums2[2])
+    println(someNums2.size)
+
+    var myFavMovies = mutableListOf<String>("Fight Club","Usual Suspects", "LA Confidential")
+    println(myFavMovies.toString())
+}
