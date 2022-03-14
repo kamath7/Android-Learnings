ba5a915bf3e171d8d332b74551574a687af93645
kamath7 <adithyakamath96@gmail.com>
2022-03-15 01:35:30 +0530

    looping added

---
+++ Kotlin/Loops/src/Loops.kt
@@ -0,0 +1,20 @@
+
+fun main() {
+    //for
+
+    for (x in 1..10){
+        println(x)
+    }
+
+    var favDishes = listOf("Palak Paneer", "Pulav","Fish Fried")
+
+    for (dish in favDishes){
+        println(dish)
+    }
+
+    for (x in 1..200){
+        if (x % 2 !=0){
+            println(x)
+        }
+    }
+}
