960f5ace4531dcf4d7f93f77d1f041819381796a
kamath7 <adithyakamath96@gmail.com>
2022-03-14 20:27:46 +0530

    some basic math and comments

---
+++ Kotlin/MathAndComments/src/MathAndCom.kt
@@ -0,0 +1,17 @@
+class MathAndCom {
+}
+
+fun main() {
+    //this is a comment
+
+    /*
+    * this is a multi line comment
+    * */
+
+    var weight = 200.00;
+    var myRadius: Int = 45
+
+    var pi: Float = 3.14F
+
+    println(myRadius * myRadius * pi)
+}
