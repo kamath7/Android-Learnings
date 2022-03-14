bd3e82576bc5a6a544d371730bdda212ec4c7949
kamath7 <adithyakamath96@gmail.com>
2022-03-14 20:34:25 +0530

    Conditional added

---
+++ Kotlin/Conditionals/src/Conditionals.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var isCarDead = true
+
+
+    if (isCarDead) {
+        println("Time to purchase a new car!")
+    }else{
+        println("Keep going ahead with the car!")
+    }
+
+    val myName: String = "Adithya"
+
+    if (myName.equals("Adithya")){
+        println(" Welcome Adithya! ")
+    }else{
+        println("Nikal Lavde")
+    }
+}
