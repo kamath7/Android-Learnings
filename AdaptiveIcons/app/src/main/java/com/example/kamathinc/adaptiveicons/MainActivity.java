cafbf94ab0b4cfdf2ab6b1bcbccd72e84fb79af2
kamath7 <adithyakamath96@gmail.com>
2022-03-18 02:31:51 +0530

    setting threshold

--- AdaptiveIcons/app/src/main/java/com/example/kamathinc/adaptiveicons/MainActivity.java
+++ AdaptiveIcons/app/src/main/java/com/example/kamathinc/adaptiveicons/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         AutoCompleteTextView autocompleteTextView = findViewById(R.id.autoCompleteTextView);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
         autocompleteTextView.setAdapter(arrayAdapter);
+        autocompleteTextView.setThreshold(1);//how many characters before suggestions
     }
 }
