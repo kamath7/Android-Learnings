ddb9be66fa368067f80563045a87d1b984f15cba
kamath7 <adithyakamath96@gmail.com>
2022-03-18 02:29:45 +0530

    autocomplete text view

--- AdaptiveIcons/app/src/main/java/com/example/kamathinc/adaptiveicons/MainActivity.java
+++ AdaptiveIcons/app/src/main/java/com/example/kamathinc/adaptiveicons/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.kamathinc.adaptiveicons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Autofill
+
+        String[] friends ={"Rhea","Amanda","Rithu"};
+
+        AutoCompleteTextView autocompleteTextView = findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
+        autocompleteTextView.setAdapter(arrayAdapter);
     }
 }
