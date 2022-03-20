51ae2ee51edba397816c68ff7f688816d6ca6836
kamath7 <adithyakamath96@gmail.com>
2022-03-21 00:20:47 +0530

    notfication setup

--- AndroidWearDemo1/app/src/main/java/com/example/kamathinc/androidweardemo1/MainActivity.java
+++ AndroidWearDemo1/app/src/main/java/com/example/kamathinc/androidweardemo1/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.kamathinc.androidweardemo1;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionService;
 import android.speech.RecognizerIntent;
+import android.support.annotation.RequiresApi;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.view.View;
@@ -26,6 +34,7 @@ public class MainActivity extends WearableActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +57,17 @@ public class MainActivity extends WearableActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         startActivityForResult(intent, 0);
 
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationChannel notificationChannel = new NotificationChannel("default", "test",NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(notificationChannel);
+
+        Intent intent1 = new Intent(this, MainActivity.class );
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent1, 0);
+
+        Notification.Builder builder = new Notification.Builder(this, "default").setContentTitle("HellO!").setContentText("Is this a great text").setSmallIcon(android.R.drawable.arrow_down_float).setContentIntent(pendingIntent);
         // Enables Always-on
+        notificationManager.notify(0, builder.build());
         setAmbientEnabled();
     }
 
