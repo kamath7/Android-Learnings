964384bee8f4ec186ce8dd9c6a32bf5a069d0a16
kamath7 <adithyakamath96@gmail.com>
2022-04-19 00:33:43 +0530

    Showing an image

--- TattiChat/app/src/main/java/com/example/kamathinc/tattichat/CreateSnapActivity.kt
+++ TattiChat/app/src/main/java/com/example/kamathinc/tattichat/CreateSnapActivity.kt
@@ -1,7 +1,12 @@
 package com.example.kamathinc.tattichat
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 
@@ -14,6 +19,44 @@ class CreateSnapActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snap)
 
+    }
 
+    fun getPhoto(){
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI )
+        startActivityForResult(intent, 1)
+    }
+
+    fun chooseImageClicked (view:View){
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        }else{
+            getPhoto()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val selectedImage = data!!.data
+
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK && data != null){
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                createSnapImageView?.setImageBitmap(bitmap)
+            }catch(e: Exception){
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1){
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getPhoto()
+            }
+        }
     }
 }
