95ada2d27e443e1d1807cf7b14f10cde21e4c548
kamath7 <adithyakamath96@gmail.com>
2022-05-27 16:32:20 +0530

    listing all snaps

--- TattiChat/app/src/main/java/com/example/kamathinc/tattichat/SnapsActivity.kt
+++ TattiChat/app/src/main/java/com/example/kamathinc/tattichat/SnapsActivity.kt
@@ -6,16 +6,50 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.google.firebase.auth.FirebaseAuth
 
 class SnapsActivity : AppCompatActivity() {
 
     val mAuth = FirebaseAuth.getInstance()
+    var snapsListView: ListView? = null
+    var emails: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snaps)
         Toast.makeText(this, "Welcome to TattiChat",Toast.LENGTH_SHORT).show()
+        snapsListView = findViewById(R.id.snapsListView)
+
+        
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_2, )
+        snapsListView?.adapter = adapter
+        FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.currentUser?.uid).child("snaps").addChildEventListener(object: ChildEventListener {
+            
+            override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
+
+                emails.add(p0.child("from")?.value as String)
+                adapter.notifyDataSetChanged()
+
+            }
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
+
+            }
+
+            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
+
+            }
+
+
+            override fun onChildRemoved(p0: DataSnapshot?) {
+
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
